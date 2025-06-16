c4c892acb21b449ce0abe3b7a9aff29c1e5f8d68
Shikasenbei07 <165635759+Shikasenbei07@users.noreply.github.com>
2025-06-17 00:55:07 +0900

    Delete GBsystem directory

--- GBsystem/src/model/GBookDBInfo.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-interface GBookDBInfo {
-	String dbUrl = "jdbc:mysql://localhost/gbookdb?serverTimezone=Asia/Tokyo";
-	String dbUser = "gbookuser";
-	String dbPass = "gbookpass";
-}
